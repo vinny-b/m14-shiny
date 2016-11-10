0be7d10d554beabe3943a123704b2db6c9507b1a
Vinny <vinayakbansal@hotmail.com>
2016-11-10 14:42:48 -0800

    D1

--- exercise-1/scripts/buildMap.R
+++ exercise-1/scripts/buildMap.R
@@ -7,10 +7,21 @@ library(plotly)
 # Try parameterize a few options, such as the title
 # I suggest: https://plot.ly/r/bubble-maps/
 BuildMap <- function(data) {
-  
-  
-  
-
-  
-  
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showland = TRUE,
+    landcolor = toRGB("gray85"),
+    subunitwidth = 1,
+    countrywidth = 1,
+    subunitcolor = toRGB("white"),
+    countrycolor = toRGB("white")
+  )
+  p <- plot_geo(df, locationmode = 'USA-states', sizes = c(1, 250)) %>%
+    add_markers(
+      x = ~lon, y = ~lat, size = ~pop, color = ~q, hoverinfo = "text",
+      text = ~paste(df$name, "<br />", df$pop/1e6, " million")
+    ) %>%
+    layout(title = title, geo = g)
+  return(p)
 }
